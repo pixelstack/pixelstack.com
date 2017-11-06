05f6d9dfca4dde390376123420333c48cb1a60b9
Jaymie Jones <jaymiejones86@gmail.com>
2017-11-06 21:01:12 +1100

    Add Base Site Content and Styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,57 +3,21 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import './reset.css'
 import './index.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div
+    className='wrapper'
+  >
     <Helmet
-      title="Gatsby Default Starter"
+      title="Pixelstack - Building solutions to your digital problems"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Website and Web Application development' },
+        { name: 'keywords', content: 'web development, web applications, react, ruby on rails' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logoSrc from '../assets/logo.svg'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <section className='home-content'>
+    <img className='brand__logo' src={logoSrc} />
+    <p>Building solutions to your digital problems<br />through websites and web applications</p>
+    <ul className='contact-list'>
+      <li><a href="mailto:hello@pixelstack.com">Contact</a></li>
+      <li><a href="twitter.com/pixelstackcom">Twitter</a></li>
+    </ul>
+  </section>
 )
 
 export default IndexPage
